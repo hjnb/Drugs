c0ba026c7ecf2a08b90e58180805fc9709af87f7
DESKTOP-OJIT7DO\yoshi <yoshitaka7144@gmail.com>
2019-04-16 11:53:50 +0900

    仕入集計：数量順、金額順印刷作成

--- Drugs/Drugs/仕入集計.vb
+++ Drugs/Drugs/仕入集計.vb
@@ -86,12 +86,102 @@ Public Class 仕入集計
     Private Sub btnExecute_Click(sender As System.Object, e As System.EventArgs) Handles btnExecute.Click
         Dim fromYmd As String = fromYmdBox.getADStr() 'from日付
         Dim toYmd As String = toYmdBox.getADStr() 'to日付
-        If rbtnSuryo.Checked Then '数量順印刷
+        If rbtnSuryo.Checked OrElse rbtnKingak.Checked Then '数量順印刷 or 金額順印刷
+            'BestWテーブル作成
+            initBestW(fromYmd, toYmd)
 
+            'データ取得
+            Dim cnn As New ADODB.Connection
+            cnn.Open(TopForm.DB_Drugs)
+            Dim rs As New ADODB.Recordset
+            Dim sql As String = ""
+            If rbtnSuryo.Checked Then
+                sql = "select Nam, Siire, Suryo, Gokei from BestW order by Suryo Desc"
+            Else
+                sql = "select Nam, Siire, Suryo, Gokei from BestW order by Gokei Desc"
+            End If
+            rs.Open(sql, cnn, ADODB.CursorTypeEnum.adOpenKeyset, ADODB.LockTypeEnum.adLockPessimistic)
+            If rs.RecordCount <= 0 Then
+                MsgBox("該当がありません。", MsgBoxStyle.Exclamation)
+                rs.Close()
+                cnn.Close()
+                Return
+            End If
+            Dim rsSum As New ADODB.Recordset()
+            sql = "select Sum(Suryo) as SS, Sum(Gokei) as SG from BestW"
+            rsSum.Open(sql, cnn, ADODB.CursorTypeEnum.adOpenKeyset, ADODB.LockTypeEnum.adLockPessimistic)
+            Dim totalSuryo As Integer = rsSum.Fields("SS").Value '合計数量
+            Dim totalGokei As Integer = rsSum.Fields("SG").Value '合計仕入金額
 
-        ElseIf rbtnKingak.Checked Then '金額順印刷
+            '書き込みデータ作成
+            Dim dataArray(49, 6) As String
+            Dim noCount As Integer = 1
+            Dim rowIndex As Integer = 0
+            Dim suryo As Integer = 0
+            Dim gokei As Integer = 0
+            While Not rs.EOF
+                dataArray(rowIndex, 0) = noCount
+                dataArray(rowIndex, 1) = Util.checkDBNullValue(rs.Fields("Nam").Value)
+                dataArray(rowIndex, 2) = Util.checkDBNullValue(rs.Fields("Siire").Value)
+                dataArray(rowIndex, 3) = CInt(rs.Fields("Suryo").Value).ToString("#,0")
+                dataArray(rowIndex, 4) = CInt(rs.Fields("Gokei").Value).ToString("#,0")
+                Dim percentStr As String = If(rbtnSuryo.Checked, formatNum(rs.Fields("Suryo").Value / totalSuryo * 100), formatNum(rs.Fields("Gokei").Value / totalGokei * 100))
+                dataArray(rowIndex, 5) = percentStr
+                suryo += CInt(rs.Fields("Suryo").Value)
+                gokei += CInt(rs.Fields("Gokei").Value)
+                If noCount Mod 5 = 0 Then
+                    dataArray(rowIndex, 6) = If(rbtnSuryo.Checked, formatNum(suryo / totalSuryo * 100), formatNum(gokei / totalGokei * 100))
+                End If
 
+                noCount += 1
+                If noCount = 51 Then
+                    Exit While
+                End If
+                rowIndex += 1
+                rs.MoveNext()
+            End While
+            rs.Close()
+            cnn.Close()
 
+            'エクセル準備
+            Dim objExcel As Excel.Application = CreateObject("Excel.Application")
+            Dim objWorkBooks As Excel.Workbooks = objExcel.Workbooks
+            Dim objWorkBook As Excel.Workbook = objWorkBooks.Open(TopForm.excelFilePass)
+            Dim oSheet As Excel.Worksheet = objWorkBook.Worksheets("仕入ベスト改")
+            objExcel.Calculation = Excel.XlCalculation.xlCalculationManual
+            objExcel.ScreenUpdating = False
+
+            'データ書き込み
+            oSheet.Range("D2").Value = If(rbtnSuryo.Checked, "仕入数量順", "仕入金額順") '区分
+            oSheet.Range("F2").Value = Util.convADStrToWarekiStr(fromYmd) & " ～ " & Util.convADStrToWarekiStr(toYmd) '期間
+            oSheet.Range("B5", "H54").Value = dataArray
+            oSheet.Range("E55").Value = suryo.ToString("#,0")
+            oSheet.Range("F55").Value = gokei.ToString("#,0")
+            oSheet.Range("G55").Value = If(rbtnSuryo.Checked, formatNum(suryo / totalSuryo * 100), formatNum(gokei / totalGokei * 100))
+            oSheet.Range("E56").Value = totalSuryo.ToString("#,0")
+            oSheet.Range("F56").Value = totalGokei.ToString("#,0")
+
+            objExcel.Calculation = Excel.XlCalculation.xlCalculationAutomatic
+            objExcel.ScreenUpdating = True
+
+            '変更保存確認ダイアログ非表示
+            objExcel.DisplayAlerts = False
+
+            '印刷
+            If TopForm.rbtnPrintout.Checked = True Then
+                oSheet.PrintOut()
+            ElseIf TopForm.rbtnPreview.Checked = True Then
+                objExcel.Visible = True
+                oSheet.PrintPreview(1)
+            End If
+
+            ' EXCEL解放
+            objExcel.Quit()
+            Marshal.ReleaseComObject(objWorkBook)
+            Marshal.ReleaseComObject(objExcel)
+            oSheet = Nothing
+            objWorkBook = Nothing
+            objExcel = Nothing
         ElseIf rbtnNam.Checked Then '品名別／月別　仕入れ数量印刷
             '対象仕入先、期間でSiireWテーブル作成
             Dim siire As String = siireBox.Text
@@ -99,7 +189,7 @@ Public Class 仕入集計
                 MsgBox("仕入先を選択して下さい。", MsgBoxStyle.Exclamation)
                 Return
             End If
-            initSiireW(siire, fromYmd, toYmd)
+            initSiireW(fromYmd, toYmd, siire)
 
             'データ取得
             Dim cnn As New ADODB.Connection
@@ -231,11 +321,25 @@ Public Class 仕入集計
     End Sub
 
     ''' <summary>
+    ''' パーセント表記の変換処理
+    ''' </summary>
+    ''' <param name="calcResult"></param>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    Private Function formatNum(calcResult As Double) As String
+        Dim result As String = Math.Round(calcResult, 1, MidpointRounding.AwayFromZero)
+        result = If(result.IndexOf(".") >= 0, result & "%", result & ".0%")
+        Return result
+    End Function
+
+    ''' <summary>
     ''' SiireWテーブル作成
     ''' </summary>
-    ''' <param name="siire"></param>
+    ''' <param name="fromYmd">from日付</param>
+    ''' <param name="toYmd">to日付</param>
+    ''' <param name="siire">仕入先</param>
     ''' <remarks></remarks>
-    Private Sub initSiireW(siire As String, fromYmd As String, toYmd As String)
+    Private Sub initSiireW(fromYmd As String, toYmd As String, Optional siire As String = "")
         '既存データ削除
         Dim cn As New ADODB.Connection()
         cn.Open(TopForm.DB_Drugs)
@@ -245,7 +349,12 @@ Public Class 仕入集計
         cmd.Execute()
 
         '仕入先データ取得
-        Dim sql As String = "select * from SiireD where Siire = '" & siire & "' and ('" & fromYmd & "' <= Ymd and Ymd <= '" & toYmd & "')"
+        Dim sql As String
+        If siire <> "" Then
+            sql = "select * from SiireD where Siire = '" & siire & "' and ('" & fromYmd & "' <= Ymd and Ymd <= '" & toYmd & "')"
+        Else
+            sql = "select * from SiireD where ('" & fromYmd & "' <= Ymd and Ymd <= '" & toYmd & "')"
+        End If
         Dim rsD As New ADODB.Recordset()
         rsD.Open(sql, cn, ADODB.CursorTypeEnum.adOpenKeyset, ADODB.LockTypeEnum.adLockPessimistic)
         If rsD.RecordCount <= 0 Then
@@ -280,6 +389,67 @@ Public Class 仕入集計
         rsW.Update()
         rsW.Close()
         rsD.Close()
+        cn.Close()
+
+    End Sub
+
+    ''' <summary>
+    ''' BestWテーブル作成
+    ''' </summary>
+    ''' <param name="fromYmd">from日付</param>
+    ''' <param name="toYmd">to日付</param>
+    ''' <remarks></remarks>
+    Private Sub initBestW(fromYmd As String, toYmd As String)
+        '指定期間のSiireWテーブル作成
+        initSiireW(fromYmd, toYmd)
+
+        '既存BestWテーブル削除
+        Dim cn As New ADODB.Connection()
+        cn.Open(TopForm.DB_Drugs)
+        Dim cmd As New ADODB.Command()
+        cmd.ActiveConnection = cn
+        cmd.CommandText = "delete from BestW"
+        cmd.Execute()
+
+        'SiireWテーブルからBestWテーブル作成
+        Dim tmpNam As String = ""
+        Dim tmpSiire As String = ""
+        Dim suryo As Integer = 0
+        Dim gokei As Integer = 0
+        Dim rsSiireW As New ADODB.Recordset()
+        Dim sql As String = "select * from SiireW order by Nam, Siire"
+        rsSiireW.Open(sql, cn, ADODB.CursorTypeEnum.adOpenKeyset, ADODB.LockTypeEnum.adLockPessimistic)
+        If rsSiireW.RecordCount <= 0 Then
+            rsSiireW.Close()
+            cn.Close()
+            Return
+        End If
+        Dim rsBestW As New ADODB.Recordset()
+        rsBestW.Open("BestW", cn, ADODB.CursorTypeEnum.adOpenKeyset, ADODB.LockTypeEnum.adLockPessimistic)
+        While Not rsSiireW.EOF
+            Dim nam As String = Util.checkDBNullValue(rsSiireW.Fields("Nam").Value)
+            Dim siire As String = Util.checkDBNullValue(rsSiireW.Fields("Siire").Value)
+            If (nam <> tmpNam) OrElse (nam = tmpNam AndAlso siire <> tmpSiire) Then
+                rsBestW.AddNew()
+                rsBestW.Fields("Nam").Value = nam
+                rsBestW.Fields("Siire").Value = siire
+
+                '更新
+                tmpNam = nam
+                tmpSiire = siire
+                suryo = 0
+                gokei = 0
+            End If
+            suryo += rsSiireW.Fields("Suryo").Value
+            gokei += rsSiireW.Fields("Gokei").Value
+            rsBestW.Fields("Suryo").Value = suryo
+            rsBestW.Fields("Gokei").Value = gokei
+
+            rsSiireW.MoveNext()
+        End While
+        rsBestW.Update()
+        rsBestW.Close()
+        rsSiireW.Close()
         cn.Close()
 
     End Sub
