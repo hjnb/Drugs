bb3e4909832ce4e27076fc0b27939764af7b0a66
DESKTOP-OJIT7DO\yoshi <yoshitaka7144@gmail.com>
2019-04-18 13:21:16 +0900

    DB整理：作成しました

--- Drugs/Drugs/ＤＢ整理.Designer.vb
+++ Drugs/Drugs/ＤＢ整理.Designer.vb
@@ -22,8 +22,78 @@ Partial Class ＤＢ整理
     'コード エディターを使って変更しないでください。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.deleteProgressBar = New System.Windows.Forms.ProgressBar()
+        Me.btnExecute = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("ＭＳ Ｐゴシック", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label1.Location = New System.Drawing.Point(35, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(348, 14)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "処理効率向上のため、次のデータの５年以前を整理します"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("ＭＳ Ｐゴシック", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label2.Location = New System.Drawing.Point(58, 79)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(72, 14)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "仕入データ"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("ＭＳ Ｐゴシック", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label3.Location = New System.Drawing.Point(58, 106)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(72, 14)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "在庫マスタ"
+        '
+        'deleteProgressBar
+        '
+        Me.deleteProgressBar.Location = New System.Drawing.Point(150, 115)
+        Me.deleteProgressBar.Name = "deleteProgressBar"
+        Me.deleteProgressBar.Size = New System.Drawing.Size(183, 13)
+        Me.deleteProgressBar.TabIndex = 3
+        '
+        'btnExecute
+        '
+        Me.btnExecute.Location = New System.Drawing.Point(370, 106)
+        Me.btnExecute.Name = "btnExecute"
+        Me.btnExecute.Size = New System.Drawing.Size(75, 33)
+        Me.btnExecute.TabIndex = 4
+        Me.btnExecute.Text = "実行"
+        Me.btnExecute.UseVisualStyleBackColor = True
+        '
+        'ＤＢ整理
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(464, 158)
+        Me.Controls.Add(Me.btnExecute)
+        Me.Controls.Add(Me.deleteProgressBar)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "ＤＢ整理"
         Me.Text = "ＤＢ整理"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents deleteProgressBar As System.Windows.Forms.ProgressBar
+    Friend WithEvents btnExecute As System.Windows.Forms.Button
 End Class
